b5e151a4ea88a5b07747e9ed52ca8718577851e2
cbigger <53627581+cbigger@users.noreply.github.com>
2021-06-15 10:19:10 -0400

    fixed command line run
    before, when run without arguments, the terminal window would close. Now it prints the usage and exits the script.

--- scripts/recon_enum/smbver.sh
+++ scripts/recon_enum/smbver.sh
@@ -7,8 +7,8 @@
 #Notes:
 # Will sometimes not capture or will print multiple
 # lines. May need to run a second time for success.
-if [ -z $1 ]; then echo "Usage: ./smbver.sh RHOST {RPORT}" && exit; else rhost=$1; fi
+if [ -z $1 ]; then echo "Usage: ./smbver.sh RHOST {RPORT}"; else rhost=$1; fi
 if [ ! -z $2 ]; then rport=$2; else rport=139; fi
 tcpdump -s0 -n -i tap0 src $rhost and port $rport -A -c 10 2>/dev/null | grep -i "samba\|s.a.m" | tr -d '.' | grep -oP 'UnixSamba.*[0-9a-z]' | tr -d '\n' & echo -n "$rhost: " &
 echo "exit" | smbclient -L $rhost 1>/dev/null 2>/dev/null
-echo "" && sleep .1
+echo "" && sleep .1;fi
